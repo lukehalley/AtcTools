9906233a92a685b0062b84fbc45ae79834740988
Luke Halley <luke123halley@gmail.com>
2019-03-01 22:31:11 +1000

    Add module docstring describing main entry point

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main entry point for AtcTools application."""
 # Application entry point for ATC tools
 """Main entry point for AtcTools application."""
 """Main entry point for AtcTools application."""
