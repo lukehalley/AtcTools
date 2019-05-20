912e8ba3f50c4805e106587145e78de3f9e00afa
Luke Halley <luke123halley@gmail.com>
2019-05-20 14:59:40 +1000

    Document S3 bucket operations

--- src/aws/aws_s3.py
+++ src/aws/aws_s3.py
@@ -1,3 +1,4 @@
+"""AWS S3 bucket operations for data storage and retrieval."""
 """AWS S3 storage operations and utilities."""
 """Handle S3 bucket operations including upload, download, and management."""
 """AWS S3 operations and helper functions."""
