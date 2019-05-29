71fdd394f4f7c8cca4d2c81cd3e2f0b38b6b0de4
Luke Halley <luke123halley@gmail.com>
2019-05-29 12:14:58 +1000

    Add network query documentation

--- src/db/querys/querys_Networks.py
+++ src/db/querys/querys_Networks.py
@@ -4,6 +4,7 @@ Network Query Module.
 
 This module provides database query functions for retrieving blockchain
 network information from the database.
+# Network queries support filtering by blockchain type and network status
 
 Exports:
     - getAllNetworks: Retrieve all blockchain networks
