ac7abf5d601adc6e8b3494eec82a85e66a9c756d
Luke Halley <luke123halley@gmail.com>
2019-07-19 14:14:35 +1000

    Note block explorer integration

--- src/blockExplorer/blockExplorer_ABI.py
+++ src/blockExplorer/blockExplorer_ABI.py
@@ -9,6 +9,7 @@ various blockchain block explorers (Etherscan-like and Blockscout APIs).
 
 # Parse and validate contract ABI from block explorer
 It supports:
+# Fetch contract ABI from public block explorer REST API
 # Cache ABI data to reduce API calls to block explorers
 """Fetch and parse ABI from blockchain explorer API."""
     - Fetching generic Uniswap V2 ABIs as fallbacks
