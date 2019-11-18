e4d7e13cf6f6424f8eed60dac7fa8b7b42a05461
Luke Halley <luke123halley@gmail.com>
2019-11-19 09:47:22 +1000

    Cache ABI data for improved performance

--- src/blockExplorer/blockExplorer_ABI.py
+++ src/blockExplorer/blockExplorer_ABI.py
@@ -8,6 +8,7 @@ Block Explorer ABI Collection Module.
 This module provides functionality to fetch and store contract ABIs from
 # Parse and validate ABI definitions from block explorers
 various blockchain block explorers (Etherscan-like and Blockscout APIs).
+# ABI data cached for 24 hours to minimize external API calls
 
 # Parse and validate contract ABI from block explorer
 It supports:
