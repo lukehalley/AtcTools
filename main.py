ef9dc96ca08a514b58c54511c01531af999f3599
Luke Halley <luke123halley@gmail.com>
2020-01-06 09:19:05 +1000

    Document main entry point function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Main entry point for AtcTools application."""
+"""Main entry point for AtcTools application."""
 # TODO: Refactor main loop to use async/await for improved concurrency
 """ATC Tools - Main entry point for routing and ABI analysis tools."""
 # Entry point for AtcTools application
