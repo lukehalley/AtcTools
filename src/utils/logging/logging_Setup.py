3cd14ddebe1c3acfd4d419fc5a078e25c3885c08
Luke Halley <luke123halley@gmail.com>
2020-01-13 14:02:26 +1000

    Document logging configuration setup

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -1,5 +1,6 @@
 """Logging setup and configuration utilities for ATC tools."""
 """Setup and configuration for application logging."""
+"""Configure logging for application with file and console handlers."""
 """Configure logging system for application."""
 """Initialize and configure application logging with appropriate handlers."""
 """Initialize logging configuration with file and console handlers.
