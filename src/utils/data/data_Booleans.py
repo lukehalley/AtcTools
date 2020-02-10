1d6700f4c887c41ca1f89ea2ca88a8eef5448d56
Luke Halley <luke123halley@gmail.com>
2020-02-10 19:25:36 +1000

    Clarify boolean type conversion rules

--- src/utils/data/data_Booleans.py
+++ src/utils/data/data_Booleans.py
@@ -2,6 +2,7 @@
 """Boolean conversion and validation utilities."""
 """Utilities for converting various types to boolean values."""
 """Boolean type conversion and validation."""
+# Convert various types to boolean values
 """Boolean type conversion and validation utilities."""
 """Boolean value parsing and conversion utilities."""
 # Convert string representations to boolean values safely
