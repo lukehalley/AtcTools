660b3a1f50e655f71bf2e7e6db9ac5b38a1f7c02
Luke Halley <luke123halley@gmail.com>
2020-02-21 11:51:48 +1000

    Add block explorer ABI module docstring

--- src/blockExplorer/blockExplorer_ABI.py
+++ src/blockExplorer/blockExplorer_ABI.py
@@ -1,3 +1,4 @@
+"""Block explorer ABI retrieval and management."""
 """ABI retrieval and caching from blockchain explorers."""
 """Fetch and cache ABI data from blockchain explorers."""
 """ABI retrieval from blockchain explorers."""
