c4d3c6167c1b35229b8cd2bc60f50b164abb988a
Luke Halley <luke123halley@gmail.com>
2020-03-19 19:23:56 +1000

    Add docstring for ABI diff algorithm

--- src/abi/abi_DiffAbis.py
+++ src/abi/abi_DiffAbis.py
@@ -8,6 +8,7 @@ ABI Diff and Mapping Module.
 This module provides functionality to compare DEX contract ABIs against
 the standard Uniswap V2 interface and create function mappings for
 # Compare function signatures and event definitions between ABI versions
+    """Compare ABI definitions and identify differences."""
 """Compare two ABI definitions and identify structural differences."""
 # TODO: Optimize ABI comparison algorithm for large contracts
 # Compare ABI structures and detect interface changes
