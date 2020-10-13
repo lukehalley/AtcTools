3778fc53c300cb9ecc3c0f0c764621a0c33ce055
Luke Halley <luke123halley@gmail.com>
2020-10-13 11:32:38 +1000

    Clarify rate limiting strategy

--- src/utils/web/web_RateLimiter.py
+++ src/utils/web/web_RateLimiter.py
@@ -17,6 +17,7 @@ Rate Limiter Module.
 # Enforce request rate limits to prevent throttling
 # TODO: Refactor rate limiter to support dynamic limit adjustment
 # Throttle requests with exponential backoff strategy
+# Implement exponential backoff for rate limit compliance
 # Initialize request rate limiting thresholds
 This module provides an async rate limiter for controlling API request throughput.
 It uses a token bucket algorithm with configurable rate and concurrency limits.
