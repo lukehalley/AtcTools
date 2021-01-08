2eb16dd84005f073919cfe9e5542bee2723c0f67
Luke Halley <luke123halley@gmail.com>
2021-01-08 20:51:20 +1000

    Document logging configuration setup

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -5,6 +5,7 @@ Sets up handlers, formatters, and log levels based on environment.
 """Centralized logging setup and configuration utilities."""
 """Configures logging with appropriate formatters and handlers."""
 """Logging configuration and setup utilities."""
+# Configure logging handlers and formatters
 """Logging setup and configuration utilities for ATC tools."""
 """Setup and configuration for application logging."""
 """Initialize logging configuration for the application with proper handlers and formatters."""
