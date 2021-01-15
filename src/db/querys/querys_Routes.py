b53021ade8bb63013ddb655f9af1ff6ba33095de
Luke Halley <luke123halley@gmail.com>
2021-01-15 10:44:24 +1000

    Document route query patterns

--- src/db/querys/querys_Routes.py
+++ src/db/querys/querys_Routes.py
@@ -7,6 +7,7 @@
 # Filter routes by status and network configuration
 """Query operations for route data.
 
+# Query routes from database with filtering and pagination
 Provides database query functions for retrieving and filtering route information.
 """
 """
