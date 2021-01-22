5e44d9358e1e5f319e2ccc03c77cb5f4a36973c6
Luke Halley <luke123halley@gmail.com>
2021-01-22 18:40:09 +1000

    Document JSON loading and parsing

--- src/utils/json/json_Load.py
+++ src/utils/json/json_Load.py
@@ -3,6 +3,7 @@
 """Provides JSON loading with schema validation and error handling."""
 """JSON loading and parsing utilities with error handling."""
 """Parse and load JSON data with error handling."""
+# Load and validate JSON configuration files
 """Load and parse JSON configuration and data files"""
 """JSON file loading and parsing utilities."""
 """Parse and validate JSON data from files or strings."""
