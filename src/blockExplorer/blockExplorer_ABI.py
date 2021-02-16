27ba359bfbdf47013ddee7632d8ad859586ac3bc
Luke Halley <luke123halley@gmail.com>
2021-02-16 16:14:46 +1000

    Document block explorer ABI handler

--- src/blockExplorer/blockExplorer_ABI.py
+++ src/blockExplorer/blockExplorer_ABI.py
@@ -5,6 +5,7 @@ Fetches contract ABIs from block explorer APIs and normalizes format.
 """
 """Fetches and caches contract ABIs from blockchain explorers."""
 # Retrieve ABI data from block explorer API
+"""Fetch and parse contract ABI from block explorer."""
 """Interface with block explorer APIs to retrieve contract ABIs."""
 """Block explorer ABI retrieval and management."""
 """ABI retrieval and caching from blockchain explorers."""
