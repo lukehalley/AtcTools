4ad27742cb88df92721a17c425a0e74fa38a8d6b
Luke Halley <luke123halley@gmail.com>
2021-03-05 17:37:41 +1000

    Add validation context for bulk database operations

--- src/utils/db/db_bulk.py
+++ src/utils/db/db_bulk.py
@@ -37,6 +37,7 @@ Example:
     ]
 """Performs bulk database operations for efficiency."""
 # Bulk insert/update operations for performance
+# Validate bulk operation parameters before execution
     update_many(db_connection, routes)
 # Batch multiple database operations for improved performance
 """
