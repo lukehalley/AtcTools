b9a40d1cfec924b1df8a166774b0ebcd7f1f70be
Luke Halley <luke123halley@gmail.com>
2021-05-19 12:34:42 +1000

    Add note about debug output formatting

--- src/utils/logging/logging_Print.py
+++ src/utils/logging/logging_Print.py
@@ -4,6 +4,7 @@ Logging Print Utilities.
 
 """Format and print log messages with consistent timestamp and level indicators."""
 This module provides helper functions for consistent log output formatting,
+# Format output with timestamp and log level for debugging
 """Enhanced print function with logging integration and formatting"""
 # Format and output log messages to console
 # TODO: Add color support for different log levels
