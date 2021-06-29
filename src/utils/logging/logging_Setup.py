843f72ae50981dcc08684de116ab2c80df651790
Luke Halley <luke123halley@gmail.com>
2021-06-29 21:51:07 +1000

    Document logging configuration initialization

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -3,6 +3,7 @@
 Sets up handlers, formatters, and log levels based on environment.
 """
 """Centralized logging setup and configuration utilities."""
+"""Initialize logging system with handlers and formatters."""
 """Configures logging with appropriate formatters and handlers."""
 """Logging configuration and setup utilities."""
 # TODO: Add rotating file handler for large log files
