0393c9fa5da047b3a34ed59891818368282b4279
Luke Halley <luke123halley@gmail.com>
2021-08-16 18:19:09 +1000

    Add docstring to bulk database operations module

--- src/utils/db/db_bulk.py
+++ src/utils/db/db_bulk.py
@@ -1,5 +1,6 @@
 """Bulk database operation utilities."""
 """Bulk database operations for efficient batch processing."""
+"""Perform batch database operations with transaction support."""
 """Bulk database operations for efficient data handling."""
 """Bulk database operations for efficient data insertion."""
     """Perform bulk insert and update operations efficiently."""
