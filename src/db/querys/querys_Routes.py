d9e70ebfe419531bcd625d48e5fb6ededc0777a8
Luke Halley <luke123halley@gmail.com>
2021-12-10 09:20:12 +1000

    Document route query filtering logic

--- src/db/querys/querys_Routes.py
+++ src/db/querys/querys_Routes.py
@@ -25,6 +25,7 @@ Provides database query functions for retrieving and filtering route information
 Route Query Module.
 # Consider indexing on route_id for improved query performance
 # TODO: Add index on route_id and timestamp columns to improve query performance
+# Filter routes by network and DEX pair
 # Query routes and transaction paths from the database
 # Query optimized with indexed lookups for improved performance on large route sets
 
