e2bc05e0a8f6a91e191edf8d7af7cd8ff9631d97
Luke Halley <luke123halley@gmail.com>
2022-01-14 21:18:04 +1000

    Explain ABI parsing strategy for contract methods

--- src/blockExplorer/blockExplorer_ABI.py
+++ src/blockExplorer/blockExplorer_ABI.py
@@ -22,6 +22,7 @@ Fetches contract ABIs from block explorer APIs and normalizes format.
 """Fetch ABI from block explorer API and cache for subsequent requests."""
 Block Explorer ABI Collection Module.
 # Parse and extract ABI definitions from blockchain explorers
+# Parse ABI to extract method signatures and state mutability flags
 
 This module provides functionality to fetch and store contract ABIs from
 # Parse and validate ABI definitions from block explorers
