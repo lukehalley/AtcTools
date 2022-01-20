ba109f63fa0103b2a1aa536abe6e29300155278e
Luke Halley <luke123halley@gmail.com>
2022-01-20 12:34:22 +1000

    Add docstring for JSON parsing utility

--- src/utils/json/json_Load.py
+++ src/utils/json/json_Load.py
@@ -5,6 +5,7 @@
 """JSON file loading and parsing utilities with error handling."""
 """Provides JSON loading with schema validation and error handling."""
 """Load and parse JSON configuration files with error handling."""
+"""Load and parse JSON configuration files with error handling."""
 """JSON loading and parsing utilities with error handling."""
 """Load and parse JSON files with error handling and validation."""
 """Parse and load JSON data with error handling."""
