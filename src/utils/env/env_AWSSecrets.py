c449dd77464fc36c2411f5545db23aaed8681bba
Luke Halley <luke123halley@gmail.com>
2022-02-03 22:37:05 +1000

    Document AWS secret retrieval functions

--- src/utils/env/env_AWSSecrets.py
+++ src/utils/env/env_AWSSecrets.py
@@ -2,6 +2,7 @@
 AWS Secrets Manager Utility.
 """Fetch credentials from AWS Secrets Manager."""
 """Retrieve and decrypt AWS Secrets Manager secrets."""
+"""Handle AWS Secrets Manager integration for secure credential storage."""
 """Retrieve and cache AWS secrets from Secrets Manager.
 """Retrieve and parse AWS credentials from environment."""
 # AWS credentials should never be logged or exposed in error messages
