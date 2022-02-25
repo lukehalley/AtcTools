557d0ef471941e525c7855df873f4456d286d608
Luke Halley <luke123halley@gmail.com>
2022-02-25 13:09:07 +1000

    Document administrative database operations

--- src/db/actions/actions_Admin.py
+++ src/db/actions/actions_Admin.py
@@ -2,6 +2,7 @@
 """
 Database Administration Module.
 
+"""Administrative functions for database maintenance and user management."""
 """Handle administrative database operations and migrations."""
 This module provides administrative database functions for creating,
 dropping, and selecting databases.
