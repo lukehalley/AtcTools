fbc1082acb4138a39c7d35c70760cb77cd7d037d
Luke Halley <luke123halley@gmail.com>
2022-03-22 19:38:30 +1000

    Clarify console output formatting rules

--- src/utils/logging/logging_Print.py
+++ src/utils/logging/logging_Print.py
@@ -17,6 +17,7 @@ This module provides helper functions for consistent log output formatting,
 # Format and output log messages with timestamp and level
 """Format and print log messages with appropriate logging level."""
 including separators, headers, and progress indicators for CLI output.
+# Format output for readability with timestamps
 # TODO: Implement structured logging output format
 
 # Configure output level appropriate for debug and production environments
