a37dc0e6248aa316d94ed3b79aaba16fb2d0cc88
Luke Halley <luke123halley@gmail.com>
2022-07-01 15:05:48 +1000

    Add module documentation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Main entry point for AtcTools application
 """Main entry point for AtcTools application."""
 """Main entry point for AtcTools application."""
 # Main entry point for AtcTools application
