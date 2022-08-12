59dafb59747ab1b2e5cc1a45c3efd99ae4593fbf
Luke Halley <luke123halley@gmail.com>
2022-08-12 17:02:52 +1000

    Explain environment variable loading

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -32,6 +32,7 @@ such as Docker containers, AWS, and headless mode.
 # Load environment variables from system and .env file
 # Load and validate environment configuration
 # Load environment variables from system configuration
+# Load configuration from .env file with fallback defaults
 
 # Load environment configuration from system
 # Load environment configuration from .env file or system variables
