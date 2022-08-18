18b2fad358bea14e058a3c9aa3de7154da2c60ef
Luke Halley <luke123halley@gmail.com>
2022-08-18 19:57:34 +1000

    Add docstring for route action handler

--- src/db/actions/actions_Routes.py
+++ src/db/actions/actions_Routes.py
@@ -17,6 +17,7 @@ including updating token associations.
 # All route mutations should validate data integrity before database operations
 # Execute database operations for route management
 # Handles route creation, modification, and deletion
+"""Execute database mutations for route management operations."""
 # Execute route update with transaction support for consistency
 # Route updates cascade to dependent records in the database
 
