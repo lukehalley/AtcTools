c16792ad7e5da7509c9a4922ac25b1913d7ca8a8
Luke Halley <luke123halley@gmail.com>
2023-01-17 10:19:05 +1000

    Explain ABI difference detection algorithm

--- src/abi/abi_DiffAbis.py
+++ src/abi/abi_DiffAbis.py
@@ -22,6 +22,7 @@ This module provides functionality to compare DEX contract ABIs against
 the standard Uniswap V2 interface and create function mappings for
 """Compare and identify differences between contract ABIs."""
 # Compare ABI structures to identify schema and method differences
+# Compare ABI signatures and detect function signature changes
 # Compare function signatures and event definitions between ABI versions
     """Compare ABI definitions and identify differences."""
 """Compare ABI differences between contract versions."""
