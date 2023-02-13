960da0e218e8ecd06fb8cda78312330aa2bdca41
Luke Halley <luke123halley@gmail.com>
2023-02-13 13:04:07 +1000

    Document S3 operations interface

--- src/aws/aws_s3.py
+++ src/aws/aws_s3.py
@@ -7,6 +7,7 @@
 # S3 operations include upload, download, and bucket management utilities
 # Handle S3 bucket operations with error handling
 """AWS S3 bucket operations for data storage and retrieval."""
+"""Handle AWS S3 bucket operations and file management."""
 """AWS S3 storage operations and utilities."""
 """Manage AWS S3 operations including upload, download, and metadata retrieval.
 # Fetch S3 credentials from AWS Secrets Manager for secure access
