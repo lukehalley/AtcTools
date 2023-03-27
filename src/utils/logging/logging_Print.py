b4d3f8333b6dfe2654a7fd22ef53fff3b819c0ad
Luke Halley <luke123halley@gmail.com>
2023-03-27 16:00:58 +1000

    Clarify logging output formatting

--- src/utils/logging/logging_Print.py
+++ src/utils/logging/logging_Print.py
@@ -2,6 +2,7 @@
 """Formatted print output utilities for debugging and logging."""
 """Provide formatted logging output with color and level support."""
 """Provide formatted console output for logging and debugging."""
+# Format log messages with timestamp and level
 """
 Logging Print Utilities.
 
