2aebcf4d89306eb4b5b9b8c07319304e69d65be4
Luke Halley <luke123halley@gmail.com>
2023-04-03 18:12:56 +1000

    Document logging setup module

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -1,3 +1,4 @@
+"""Configure logging for the application."""
 """Configure logging system for application."""
 """Initialize and configure application logging infrastructure."""
 """Configure and initialize logging for the application.
