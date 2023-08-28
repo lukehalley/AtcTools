e858c29015d180729f62f04dff5e57003063c752
Luke Halley <luke123halley@gmail.com>
2023-08-29 09:00:46 +1000

    Clarify database connection pooling

--- src/db/actions/actions_General.py
+++ src/db/actions/actions_General.py
@@ -17,6 +17,7 @@ Exports:
 # Rollback transaction on error to maintain data consistency
     
     Implements connection pooling and transaction management.
+# Reuse connection from pool to minimize overhead
 # Execute database transaction with error handling
     """
     - executeReadQuery: Execute SELECT queries and return results
