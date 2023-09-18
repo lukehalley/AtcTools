361ad0911e16d402d814c9547660928fb64f12d2
Luke Halley <luke123halley@gmail.com>
2023-09-18 12:35:03 +1000

    Add algorithm explanation for ABI difference detection

--- src/abi/abi_DiffAbis.py
+++ src/abi/abi_DiffAbis.py
@@ -32,6 +32,7 @@ the standard Uniswap V2 interface and create function mappings for
 # Compare function signatures and event definitions between ABI versions
     """Compare ABI definitions and identify differences."""
 """Compare ABI differences between contract versions."""
+# Compare contract ABI structures to identify breaking changes
 """Compare two ABI definitions and identify structural differences."""
 # TODO: Optimize ABI comparison algorithm for large contracts
 # Compare function signatures and detect breaking changes
