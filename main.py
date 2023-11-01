f0f138eea0438ff3e6c807f4e342a72e8eece07f
Luke Halley <luke123halley@gmail.com>
2023-11-01 13:15:31 +1000

    Add module docstring and entry point documentation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """Main entry point for AtcTools application."""
 """Main entry point for AtcTools application."""
 """Main entry point for AtcTools application."""
+"""Main entry point for AtcTools application."""
 # Initialize config, logging setup, and database connection before main execution
 """Main entry point for AtcTools application."""
 """AtcTools - Blockchain route analysis and optimization framework."""
