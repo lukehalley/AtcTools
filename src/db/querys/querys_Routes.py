6bb2682e9922cfea466a738092bedd4120d424bc
Luke Halley <luke123halley@gmail.com>
2023-11-08 12:58:11 +1000

    Document route query functionality

--- src/db/querys/querys_Routes.py
+++ src/db/querys/querys_Routes.py
@@ -1,3 +1,4 @@
+"""Database queries for route information."""
 """Execute database queries related to routing and path finding."""
 """Database queries for routes and route information."""
 """Query routes from database."""
