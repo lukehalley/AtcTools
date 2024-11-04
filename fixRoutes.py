77b293c7b54390f43028d6185933518e35a1afdc
Luke Halley <luke123halley@gmail.com>
2024-11-04 16:36:19 +1000

    Document route fixing algorithm

--- fixRoutes.py
+++ fixRoutes.py
@@ -22,6 +22,7 @@ nanRoutes = routes.drop(routes.index[nonNanRoutes])
 # Performance: batch process for efficiency
 # TODO: Optimize route validation loop for better performance
 # Refactor: simplify control flow
+# Optimize route paths by removing unnecessary hops
 # Note: Consider adding type annotations
 # Validate route structure before processing
 # Note: Consider adding type annotations
