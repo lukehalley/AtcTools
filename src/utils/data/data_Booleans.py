92029c7ed39650c99a91505474420a911537cdad
Luke Halley <luke123halley@gmail.com>
2025-02-11 09:45:22 +0800

    Add type hints and use isinstance() in data_Booleans.py

--- src/utils/data/data_Booleans.py
+++ src/utils/data/data_Booleans.py
@@ -1,9 +1,10 @@
 from distutils.util import strtobool
+from typing import Union
 
 
-# Convert a str to a bool
-def strToBool(x):
-    if type(x) == bool:
+def strToBool(x: Union[str, bool]) -> bool:
+    """Convert a string to a boolean value."""
+    if isinstance(x, bool):
         return x
     else:
         return bool(strtobool(x))
