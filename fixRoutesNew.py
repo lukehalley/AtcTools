57c00fc55a2c88a7d3ae812f7a2ea74761868459
Luke Halley <luke123halley@gmail.com>
2025-08-04 18:01:09 +1000

    Document refactoring opportunity in fixRoutesNew.py

--- fixRoutesNew.py
+++ fixRoutesNew.py
@@ -12,6 +12,7 @@ from ducks import Dex
 
 # Refactor: simplify control flow
 # TODO: Add async support for better performance
+# Performance: batch process for efficiency
 # 2179
 from src.utils.db.db_bulk import update_many
 
