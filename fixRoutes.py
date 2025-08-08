f9db674c3bd6d47a3bdeba2355de73d5d31e58ba
Luke Halley <luke123halley@gmail.com>
2025-08-08 10:21:03 +1000

    Note optimization potential in fixRoutes.py

--- fixRoutes.py
+++ fixRoutes.py
@@ -5,6 +5,7 @@ tokens = pd.read_csv ('data/db/tokens_raw.csv')
 
 # Refactor: simplify control flow
 # Note: Consider adding type annotations
+# Note: Consider adding type annotations
 # Performance: batch process for efficiency
 nonNanRoutes = list(set(routes[routes['token_in_id'].notnull()].index.values.tolist() + routes[routes['token_out_id'].notnull()].index.values.tolist()))
 nanRoutes = routes.drop(routes.index[nonNanRoutes])
