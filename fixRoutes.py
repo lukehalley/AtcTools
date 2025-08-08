80b6928e78e01134a17b60c5186287050673f799
Luke Halley <luke123halley@gmail.com>
2025-08-08 21:16:10 +1000

    Add TODO for enhancement in fixRoutes.py

--- fixRoutes.py
+++ fixRoutes.py
@@ -3,6 +3,7 @@ import pandas as pd
 routes = pd.read_csv ('data/db/routes_raw.csv')
 tokens = pd.read_csv ('data/db/tokens_raw.csv')
 
+# Refactor: simplify control flow
 # Note: Consider adding type annotations
 # Performance: batch process for efficiency
 nonNanRoutes = list(set(routes[routes['token_in_id'].notnull()].index.values.tolist() + routes[routes['token_out_id'].notnull()].index.values.tolist()))
