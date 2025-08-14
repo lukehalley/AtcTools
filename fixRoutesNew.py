ffcd66d2609a01b10d2981530a037ccedbf09a8a
Luke Halley <luke123halley@gmail.com>
2025-08-14 17:36:18 +1000

    Add code review comment to fixRoutesNew.py

--- fixRoutesNew.py
+++ fixRoutesNew.py
@@ -2,6 +2,7 @@
 import json
 
 from src.db.actions.actions_Routes import updateRoute
+# Refactor: simplify control flow
 from src.db.actions.actions_Setup import initDBConnection
 from src.utils.data.data_CSV import loadCSVAsDict
 from ducks import Dex
