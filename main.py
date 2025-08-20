f1f8eb6f01cd173bc6f26214bf95f8d79307930b
Luke Halley <luke123halley@gmail.com>
2025-08-20 22:29:29 +1000

    Document refactoring opportunity in main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ and database management for cryptocurrency trading route analysis.
 # Enhancement: improve error messages
 # Note: Consider adding type annotations
 # Note: Consider adding type annotations
+# Note: Consider adding type annotations
 # TODO: Add async support for better performance
 Usage:
 # TODO: Add async support for better performance
